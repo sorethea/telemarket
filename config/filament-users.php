0434a66f3825bf0e33300100fa5d9b3aa78e6aab
Sorethea OK <sorethea@gmail.com>
2024-09-03 15:13:20 +0700

    first commit

--- config/filament-users.php
+++ config/filament-users.php
@@ -6,7 +6,7 @@ return [
      * If you want to use your own resource, you can set this to true.
      * and use `php artisan filament-user:publish` to publish the resource.
      */
-    "publish_resource" => false,
+    "publish_resource" => true,
 
     /*
      * The Group name of the resource.

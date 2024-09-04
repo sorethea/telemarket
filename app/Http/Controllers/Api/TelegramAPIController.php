eb827c371c16b91886461cf4690d35de5fbe2de8
Sorethea OK <sorethea@gmail.com>
2024-09-04 16:03:31 +0700

    first commit

--- app/Http/Controllers/Api/TelegramAPIController.php
+++ app/Http/Controllers/Api/TelegramAPIController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer;
 use App\Models\Message;
 use Illuminate\Http\Request;
 use Telegram\Bot\Laravel\Facades\Telegram;
@@ -19,6 +20,14 @@ class TelegramAPIController extends Controller
         $message->type = $chat->get("type");
         $message->message = $msg;
         $message->save();
-        return 'ok';
+        $from = $update->get('from');
+        $customer = Customer::where('telegram_id',$from->get('id'))->first();
+        if(empty($customer)){
+            $customer = new Customer();
+            $customer->telegram_id=$from->get('id');
+            $customer->first_name=$from->get('first_name');
+            $customer->last_name=$from->get('last_name');
+            $customer->save();
+        }
     }
 }

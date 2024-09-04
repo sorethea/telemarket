4cff8c62e4dfbd1fb1a82ce4f9c687d704ca16f8
Sorethea OK <sorethea@gmail.com>
2024-09-04 09:53:18 +0700

    first commit

--- app/Filament/Auth/Login.php
+++ app/Filament/Auth/Login.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Filament\Auth;
+use Filament\Forms\Components\Component;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Pages\Auth\Login as AuthLogin;
 class Login extends AuthLogin
@@ -12,5 +14,24 @@ class Login extends AuthLogin
             $this->getRememberFormComponent()
         ])
             ->statePath('data');
+    }
+    protected function getLoginFormComponent(): Component
+    {
+        return TextInput::make('login')
+            ->label('Login')
+            ->required()
+            ->autocomplete()
+            ->autofocus()
+            ->extraInputAttributes(['tabindex' => 1]);
+    }
+
+    protected function getCredentialsFromFormData(array $data): array
+    {
+        $login_type = filter_var($data['login'], FILTER_VALIDATE_EMAIL ) ? 'email' : 'phone_number';
+
+        return [
+            $login_type => $data['login'],
+            'password'  => $data['password'],
+        ];
     }
 }

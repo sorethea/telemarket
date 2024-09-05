f5224da29755e24be955ea0fd7c7ce3519f0e244
Sorethea OK <sorethea@gmail.com>
2024-09-05 15:15:04 +0700

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Customer;
+use App\Policies\CustomerPolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::policy(Customer::class,CustomerPolicy::class);
     }
 }

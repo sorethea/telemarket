b27a9f0d62a8a10173842921b504ac81993a856a
Sorethea OK <sorethea@gmail.com>
2024-09-05 09:45:46 +0700

    first commit

--- app/Console/Commands/Telegram/Start.php
+++ app/Console/Commands/Telegram/Start.php
@@ -20,7 +20,7 @@ class Start extends Command
             'text' => __('command.start', ['bot'=>$bot]),
             'reply_markup'=>json_encode([
                 'inline_keyboard'=>[
-                    [['text'=>'Subscript','callback_data'=>'subscript', "request_contact"=>true,'border'=>true]],
+                    [['text'=>'Subscript','callback_data'=>'subscript']],
                 ],
                 'keyboard'=>[
                     [['text'=>'Subscript','callback_data'=>'subscript', "request_contact"=>true,'border'=>true]],

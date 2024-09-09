15c126fb62f409a89deffb0e95e1376e07df7382
Sorethea OK <sorethea@gmail.com>
2024-09-09 14:13:03 +0700

    first commit

--- app/Filament/Marketing/Resources/PostResource.php
+++ app/Filament/Marketing/Resources/PostResource.php
@@ -18,7 +18,7 @@ class PostResource extends Resource implements HasShieldPermissions
 {
     protected static ?string $model = Post::class;
 
-    protected static ?string $navigationIcon = 'gmdi-list_alt';
+    protected static ?string $navigationIcon = 'heroicon-o-photo';
 
     public static function getNavigationGroup(): string
     {

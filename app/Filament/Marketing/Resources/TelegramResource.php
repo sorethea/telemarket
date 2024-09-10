4b69b1596df6507a7636538abec15b999a4e70ac
Sorethea OK <sorethea@gmail.com>
2024-09-10 10:59:33 +0700

    first commit

--- app/Filament/Marketing/Resources/TelegramResource.php
+++ app/Filament/Marketing/Resources/TelegramResource.php
@@ -79,7 +79,7 @@ class TelegramResource extends Resource implements HasShieldPermissions
 
                 Tables\Columns\TextColumn::make('status')
                     ->label(trans('market.telegram.status.title'))
-                    ->badge(Status::class)
+                    ->color(Status::class)
                     ->searchable()
                     ->sortable(),
             ])

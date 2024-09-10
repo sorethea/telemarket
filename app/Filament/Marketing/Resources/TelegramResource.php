a6f1d8b3d6f7aada62c1ceb92aedb4910f415206
Sorethea OK <sorethea@gmail.com>
2024-09-10 15:31:53 +0700

    first commit

--- app/Filament/Marketing/Resources/TelegramResource.php
+++ app/Filament/Marketing/Resources/TelegramResource.php
@@ -41,10 +41,11 @@ class TelegramResource extends Resource implements HasShieldPermissions
                     Forms\Components\TextInput::make("title")
                         ->label(trans('market.telegram.title'))
                         ->required(),
-//                Forms\Components\Select::make("status")
-//                    ->label(trans('market.telegram.status.title'))
-//                    ->options(Status::class)
-//                    ->required(),
+                Forms\Components\Select::make("status")
+                    ->label(trans('market.telegram.status.title'))
+                    ->options(Status::class)
+                    ->default('draft')
+                    ->required(),
 
                     Forms\Components\MarkdownEditor::make('content')
                         ->label(trans('market.telegram.content'))

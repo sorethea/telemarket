70eadfd79e6c89e7b7217b3e764671c6e36ff073
Sorethea OK <sorethea@gmail.com>
2024-09-11 09:33:26 +0700

    first commit

--- database/migrations/2024_09_11_021559_create_commands_table.php
+++ database/migrations/2024_09_11_021559_create_commands_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string("name");
             $table->text("text")->nullable();
-            $table->text("photo")->nullable();
+            $table->text("photos")->nullable();
             $table->string("bot")->nullable();
             $table->timestamps();
         });

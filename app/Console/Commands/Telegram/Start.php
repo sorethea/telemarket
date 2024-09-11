8d37bbc32e019a20d63758a5f72153c49318dd9d
Sorethea OK <sorethea@gmail.com>
2024-09-11 10:58:26 +0700

    first commit

--- app/Console/Commands/Telegram/Start.php
+++ app/Console/Commands/Telegram/Start.php
@@ -19,16 +19,18 @@ class Start extends Command
 
     public function handle(): void
     {
-        $bot = Request::get('bot');
+        $bot = Request::get('bot',config('telegram.default'));
         $telegram = Telegram::bot($bot);
         $botName = config('telegram.bots.'.$bot.'.name');
         $botWebhookUrl = config('telegram.bots.'.$bot.'.webhook_url');
         $telegram->setWebhook(['url'=>$botWebhookUrl]);
-        $posts = Post::query()->limit(3)->get();
-        foreach ($posts as $post){
-            $this->replyWithPhoto([
-                "photo"=>InputFile::create("storage/".$post->photos)
-            ]);
+        $startCommandObj = \App\Models\Command::query()->where('name','start')->where('bot',$bot)->first();
+        if(!empty($photos = $startCommandObj->photos)){
+            foreach ($photos as $photo){
+                $this->replyWithPhoto([
+                    "photo"=>InputFile::create('storage/'.$photo),
+                ]);
+            }
         }
         $this->replyWithMessage([
             'text' => __('command.start', ['bot'=>$botName]),

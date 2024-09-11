683d51ebeaabe5b66a50532a3a941b6db02d678f
Sorethea OK <sorethea@gmail.com>
2024-09-11 16:34:06 +0700

    first commit

--- app/Filament/Auth/Register.php
+++ app/Filament/Auth/Register.php
@@ -1,15 +1,35 @@
 <?php
 
 namespace App\Filament\Auth;
+use Filament\Forms\Components\Component;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
 use Filament\Pages\Auth\Register as BaseRegister;
 class Register extends BaseRegister
 {
-    /**
-     * @return string|null
-     */
-    public function getTitle(): string
+
+    protected function getForms(): array
     {
-        return config('telegram.bots.ichiban.name');
+        return [
+            'form' => $this->form(
+                $this->makeForm()
+                    ->schema([
+                        $this->getNameFormComponent(),
+                        $this->getPhoneNumberFormComponent(),
+                        $this->getPasswordFormComponent(),
+                        $this->getPasswordConfirmationFormComponent(),
+                    ])
+                    ->statePath('data'),
+            ),
+        ];
     }
 
+    protected function getPhoneNumberFormComponent(): Component
+    {
+        return TextInput::make('phone_number')
+            ->label(__('general.contact.phone_number'))
+            ->required()
+            ->maxLength(255)
+            ->unique($this->getUserModel());
+    }
 }

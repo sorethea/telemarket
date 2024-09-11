9afc6e642cdb032cfd6f94406edfe6aade384590
Sorethea OK <sorethea@gmail.com>
2024-09-11 16:19:41 +0700

    first commit

--- app/Filament/Auth/Register.php
+++ app/Filament/Auth/Register.php
@@ -4,5 +4,12 @@ namespace App\Filament\Auth;
 use Filament\Pages\Auth\Register as BaseRegister;
 class Register extends BaseRegister
 {
+    /**
+     * @return string|null
+     */
+    public function getTitle(): string
+    {
+        return config('telegram.bots.ichiban.name');
+    }
 
 }

04d343da7b4413339185bf19474d5084edc904a8
Sorethea OK <sorethea@gmail.com>
2024-09-13 10:22:22 +0700

    first commit

--- app/Filament/Marketing/Resources/TelegramResource.php
+++ app/Filament/Marketing/Resources/TelegramResource.php
@@ -114,7 +114,7 @@ class TelegramResource extends Resource implements HasShieldPermissions
                         foreach ($customers as $customer){
                             $data[] = $customer->first_name." ".$customer->last_name;
                         }
-                        return implode("&#013;",$data);
+                        return nl2br(implode("<br>",$data));
                     })
                     ->label(trans('market.telegram.send_to')),
             ])

b8a63f4520ec055594f33b44b127dab0e032a4aa
Sorethea OK <sorethea@gmail.com>
2024-09-18 15:01:28 +0700

    commit 18 Sep 2024

--- config/filament.php
+++ config/filament.php
@@ -18,11 +18,11 @@ return [
 
          'echo' => [
              'broadcaster' => 'reverb',
-             'key' => env('VITE_PUSHER_APP_KEY'),
-             'cluster' => env('VITE_PUSHER_APP_CLUSTER'),
-             'wsHost' => env('VITE_PUSHER_HOST'),
-             'wsPort' => env('VITE_PUSHER_PORT'),
-             'wssPort' => env('VITE_PUSHER_PORT'),
+             'key' => env('VITE_REVERB_APP_KEY'),
+             'cluster' => env('VITE_REVERB_APP_CLUSTER'),
+             'wsHost' => env('VITE_REVERB_HOST'),
+             'wsPort' => env('VITE_REVERB_PORT'),
+             'wssPort' => env('VITE_REVERB_PORT'),
              'authEndpoint' => '/broadcasting/auth',
              'disableStats' => true,
              'encrypted' => true,

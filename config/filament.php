c0a3a0e1abd3d5530daceae478ad073a88e419fd
Sorethea OK <sorethea@gmail.com>
2024-09-18 15:06:35 +0700

    commit 18 Sep 2024

--- config/filament.php
+++ config/filament.php
@@ -26,7 +26,7 @@ return [
              'authEndpoint' => '/broadcasting/auth',
              'disableStats' => true,
              'encrypted' => true,
-             'forceTLS' => false,
+             'forceTLS' => true,
          ],
 
     ],

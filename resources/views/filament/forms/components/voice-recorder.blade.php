4914b3f2330b130518950156b383d5fdac1e46dd
Sorethea OK <sorethea@gmail.com>
2024-09-20 15:36:58 +0700

    commit 20 Sep 2024

--- resources/views/filament/forms/components/voice-recorder.blade.php
+++ resources/views/filament/forms/components/voice-recorder.blade.php
@@ -1,4 +1,6 @@
 <div>
-    <x-filament-forms::field-wrapper.label>Voice Recorder</x-filament-forms::field-wrapper.label>
-    <x-filament::button icon="heroicon-o-microphone"></x-filament::button>
+    <x-filament-forms::field-wrapper.label>Voice:
+        <x-filament::button icon="heroicon-o-microphone"></x-filament::button>
+    </x-filament-forms::field-wrapper.label>
+
 </div>

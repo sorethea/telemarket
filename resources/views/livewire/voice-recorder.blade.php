942e2bd161d2963f970b2459c52fbb1e4f7f829f
Sorethea OK <sorethea@gmail.com>
2024-09-25 14:51:17 +0700

    commit to hieat

--- resources/views/livewire/voice-recorder.blade.php
+++ resources/views/livewire/voice-recorder.blade.php
@@ -1,10 +1,10 @@
 <div>
     <h1>Voice Recorder</h1>
-    <x-filament::button icon="heroicon-o-microphone" wire:click="js:voiceRecord();"/>
+    <x-filament::button icon="heroicon-o-microphone" onclick="voiceRecord();"/>
 </div>
 @script
 <script>
-    function voiceRecord(){
+    function voiceRecord() {
         alert("Voice Record!")
     }
 </script>

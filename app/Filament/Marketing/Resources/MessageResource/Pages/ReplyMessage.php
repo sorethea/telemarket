07513a700b0c0f6a9679a5746dbb6e17a0d0a28c
Sorethea OK <sorethea@gmail.com>
2024-09-25 15:44:15 +0700

    commit to hieat

--- app/Filament/Marketing/Resources/MessageResource/Pages/ReplyMessage.php
+++ app/Filament/Marketing/Resources/MessageResource/Pages/ReplyMessage.php
@@ -3,6 +3,9 @@
 namespace App\Filament\Marketing\Resources\MessageResource\Pages;
 
 use App\Filament\Marketing\Resources\MessageResource;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Form;
 use Filament\Resources\Pages\Page;
 
 class ReplyMessage extends Page
@@ -11,28 +14,28 @@ class ReplyMessage extends Page
 
     protected static string $view = 'filament.marketing.resources.message-resource.pages.reply-message';
 
-    protected function getForms(): array
+    public function form(Form $form): Form
     {
-        return [
-            Forms\Components\MarkdownEditor::make('text')
-                                ->required(fn($get)=>empty($get("file"))),
-                            Forms\Components\FileUpload::make('file')
-                                ->multiple()
-                                ->disk('public')
-                                ->acceptedFileTypes([
-                                    'image/jpg',
-                                    'image/npg',
-                                    'image/gif',
-                                    'audio/ogg',
-                                    'audio/oga',
-                                    'video/mpeg',
-                                    'video/mp4',
-                                    'application/pdf',
-                                    'application/zip',
-                                    'text/plain'
-                                ])
-                                ->directory(fn($record)=>$record->customer_id.'/sent')
-                                ->required(fn($get)=>empty($get("text"))),
-        ];
+        return $form->schema([
+            MarkdownEditor::make('text')
+                ->required(fn($get)=>empty($get("file"))),
+            FileUpload::make('file')
+                ->multiple()
+                ->disk('public')
+                ->acceptedFileTypes([
+                    'image/jpg',
+                    'image/npg',
+                    'image/gif',
+                    'audio/ogg',
+                    'audio/oga',
+                    'video/mpeg',
+                    'video/mp4',
+                    'application/pdf',
+                    'application/zip',
+                    'text/plain'
+                ])
+                ->directory(fn($record)=>$record->customer_id.'/sent')
+                ->required(fn($get)=>empty($get("text"))),
+        ]);
     }
 }

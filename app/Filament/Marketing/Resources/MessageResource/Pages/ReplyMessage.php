7225b7c07fc268965c3085b1ee201ef0d62e39ef
Sorethea OK <sorethea@gmail.com>
2024-09-25 15:19:53 +0700

    commit to hieat

--- app/Filament/Marketing/Resources/MessageResource/Pages/ReplyMessage.php
+++ app/Filament/Marketing/Resources/MessageResource/Pages/ReplyMessage.php
@@ -8,6 +8,7 @@ use Filament\Resources\Pages\Page;
 class ReplyMessage extends Page
 {
     protected static string $resource = MessageResource::class;
+    protected static ?string $navigationIcon = 'heroicon-o-arrow-uturn-left';
 
     protected static string $view = 'filament.marketing.resources.message-resource.pages.reply-message';
 }

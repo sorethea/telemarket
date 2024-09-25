3b1e92a9c23d7618f0566aea5c415460d5cc293f
Sorethea OK <sorethea@gmail.com>
2024-09-25 11:52:22 +0700

    commit to hieat

--- app/Forms/Components/VoiceRecorder.php
+++ app/Forms/Components/VoiceRecorder.php
@@ -3,8 +3,13 @@
 namespace App\Forms\Components;
 
 use Filament\Forms\Components\Field;
+use Livewire\Livewire;
 
 class VoiceRecorder extends Field
 {
     protected string $view = 'forms.components.voice-recorder';
+
+    public function voiceRecord(){
+        dispatch('vice-record');
+    }
 }
--- resources/views/forms/components/voice-recorder.blade.php
+++ resources/views/forms/components/voice-recorder.blade.php
@@ -4,6 +4,13 @@
 >
     <div x-data="{ state: $wire.$entangle('{{ $getStatePath() }}') }">
         <!-- Interact with the `state` property in Alpine.js -->
-        <x-filament::button  icon="heroicon-o-microphone" tooltip="Voice Recorder"></x-filament::button>
+        <x-filament::button  icon="heroicon-o-microphone" wire:click.prevent="voiceRecord" tooltip="Voice Recorder"></x-filament::button>
     </div>
 </x-dynamic-component>
+@script
+<script>
+    function viceRecord(){
+        alert("Start record!");
+    }
+</script>
+@endscript

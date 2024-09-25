1f2d51cb1e660fb2b41905c403e6b73bb30fa75f
Sorethea OK <sorethea@gmail.com>
2024-09-25 10:54:26 +0700

    commit to hieat

--- resources/views/forms/components/voice-recorder.blade.php
+++ resources/views/forms/components/voice-recorder.blade.php
@@ -4,5 +4,6 @@
 >
     <div x-data="{ state: $wire.$entangle('{{ $getStatePath() }}') }">
         <!-- Interact with the `state` property in Alpine.js -->
+        <x-filament::button  icon="heroicon-o-microphone" tooltip="Voice Recorder"></x-filament::button>
     </div>
 </x-dynamic-component>

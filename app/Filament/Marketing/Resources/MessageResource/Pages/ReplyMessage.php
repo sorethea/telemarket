2445dd353007e83fcc56fec40150f7a0c9a5d009
Sorethea OK <sorethea@gmail.com>
2024-09-25 16:05:28 +0700

    commit to hieat

--- app/Filament/Marketing/Resources/MessageResource/Pages/ReplyMessage.php
+++ app/Filament/Marketing/Resources/MessageResource/Pages/ReplyMessage.php
@@ -18,9 +18,9 @@ class ReplyMessage extends Page implements HasForms
 
     protected static string $view = 'filament.marketing.resources.message-resource.pages.reply-message';
 
-    public function form(Form $form): Form
+    protected function getFormSchema(): array
     {
-        return $form->schema([
+        return [
             MarkdownEditor::make('text')
                 ->required(fn($get)=>empty($get("file"))),
             FileUpload::make('file')
@@ -40,6 +40,6 @@ class ReplyMessage extends Page implements HasForms
                 ])
                 ->directory(fn($record)=>$record->customer_id.'/sent')
                 ->required(fn($get)=>empty($get("text"))),
-        ]);
+        ];
     }
 }

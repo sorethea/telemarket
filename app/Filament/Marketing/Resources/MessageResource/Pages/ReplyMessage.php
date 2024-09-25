92613be41311c6e3aa1706f664a8b9e3127959b3
Sorethea OK <sorethea@gmail.com>
2024-09-25 15:39:00 +0700

    commit to hieat

--- app/Filament/Marketing/Resources/MessageResource/Pages/ReplyMessage.php
+++ app/Filament/Marketing/Resources/MessageResource/Pages/ReplyMessage.php
@@ -10,4 +10,29 @@ class ReplyMessage extends Page
     protected static string $resource = MessageResource::class;
 
     protected static string $view = 'filament.marketing.resources.message-resource.pages.reply-message';
+
+    protected function getForms(): array
+    {
+        return [
+            Forms\Components\MarkdownEditor::make('text')
+                                ->required(fn($get)=>empty($get("file"))),
+                            Forms\Components\FileUpload::make('file')
+                                ->multiple()
+                                ->disk('public')
+                                ->acceptedFileTypes([
+                                    'image/jpg',
+                                    'image/npg',
+                                    'image/gif',
+                                    'audio/ogg',
+                                    'audio/oga',
+                                    'video/mpeg',
+                                    'video/mp4',
+                                    'application/pdf',
+                                    'application/zip',
+                                    'text/plain'
+                                ])
+                                ->directory(fn($record)=>$record->customer_id.'/sent')
+                                ->required(fn($get)=>empty($get("text"))),
+        ];
+    }
 }

b746656791f73a02821ac7824736f9242737d046
Sorethea OK <sorethea@gmail.com>
2024-09-25 17:02:56 +0700

    commit to hieat

--- resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
+++ resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
@@ -1,8 +1,5 @@
 <x-filament-panels::page>
 <x-filament-panels::form>
     {{$this->form}}
-    <x-filament-forms::actions>
-        {{$this->mountedActions}}
-    </x-filament-forms::actions>
 </x-filament-panels::form>
 </x-filament-panels::page>

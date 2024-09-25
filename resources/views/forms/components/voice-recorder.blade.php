174110176f13fb6faafd132b6643e4eb457e055e
Sorethea OK <sorethea@gmail.com>
2024-09-25 13:33:48 +0700

    commit to hieat

--- resources/views/forms/components/voice-recorder.blade.php
+++ resources/views/forms/components/voice-recorder.blade.php
@@ -1,11 +1,11 @@
 <x-dynamic-component
     :component="$getFieldWrapperView()"
     :field="$field"
-    wire:click.prevent="$voiceRecord"
+
 >
     <div x-data="{ state: $wire.$entangle('{{ $getStatePath() }}') }">
         <!-- Interact with the `state` property in Alpine.js -->
-        <x-filament::button  icon="heroicon-o-microphone" tooltip="Voice Recorder"></x-filament::button>
+        <x-filament::button wire:click.prevent="voiceRecord"  icon="heroicon-o-microphone" tooltip="Voice Recorder"></x-filament::button>
     </div>
 </x-dynamic-component>
 @script

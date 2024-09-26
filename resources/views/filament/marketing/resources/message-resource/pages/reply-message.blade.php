02b6635ab589fb6a2778277539fa3267161f5d06
Sorethea OK <sorethea@gmail.com>
2024-09-26 14:09:23 +0700

    commit to hieat

--- resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
+++ resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
@@ -27,7 +27,7 @@
 @script
 <script>
     window.addEventListener('voiceRecord',(event)=>{
-        voice_record(event['__livewire']['params']['message']);
+        voice_record(event['__livewire']['params'][0]['message']);
     });
     function voice_record(event){
         alert(JSON.stringify(event));

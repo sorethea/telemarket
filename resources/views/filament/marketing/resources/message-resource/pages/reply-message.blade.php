0d1cccfc42a1320b18bd7ec08f00f9394166760f
Sorethea OK <sorethea@gmail.com>
2024-09-26 09:13:54 +0700

    commit to hieat

--- resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
+++ resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
@@ -2,7 +2,8 @@
 <x-filament-panels::form>
     {{$this->form}}
     <x-filament::button maxlength="35">@lang("market.message.reply")</x-filament::button>
-    <x-filament-panels::form.actions>
-    </x-filament-panels::form.actions>
+    <x-filament-panels::form.actions
+        :actions="$this->getActions()"
+    />
 </x-filament-panels::form>
 </x-filament-panels::page>

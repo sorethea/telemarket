e6e16121aa97809d7d084a3a4deea3f04bbf99f3
Sorethea OK <sorethea@gmail.com>
2024-09-26 09:26:35 +0700

    commit to hieat

--- resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
+++ resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
@@ -1,6 +1,7 @@
 <x-filament-panels::page>
 <x-filament-panels::form>
     {{$this->form}}
+    <x-filament::button icon="heroicon-o-microphone"/>
     <x-filament-panels::form.actions
         alignment="right"
         :actions="$this->getFormActions()"

ddc38a693bc00920c46480208e7640a517729708
Sorethea OK <sorethea@gmail.com>
2024-09-26 15:41:07 +0700

    commit to hieat

--- resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
+++ resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
@@ -29,8 +29,102 @@
     window.addEventListener('voiceRecord',(event)=>{
         voice_record(event['__livewire']['params'][0]['message']);
     });
-    function voice_record(event){
-        alert(JSON.stringify(event));
+    let mediaRecorder;
+
+    let audioChunks = [];
+
+    function voice_record(){
+        navigator.mediaDevices.getUserMedia({ audio: true })
+
+            .then(stream => {
+
+                mediaRecorder = new MediaRecorder(stream);
+
+                mediaRecorder.start();
+
+                // document.getElementById('startRecording').disabled = true;
+                //
+                // document.getElementById('stopRecording').disabled = false;
+
+                // mediaRecorder.addEventListener('dataavailable', event => {
+                //
+                //     audioChunks.push(event.data);
+                //
+                // });
+
+                // mediaRecorder.addEventListener('stop', () => {
+                //
+                //     const audioBlob = new Blob(audioChunks);
+                //
+                //     const audioUrl = URL.createObjectURL(audioBlob);
+                //
+                //     const audio = document.getElementById('audioPlayback');
+                //
+                //     audio.src = audioUrl;
+                //
+                //     uploadAudio(audioBlob);
+                //
+                // });
+
+            });
     }
+
+
+    document.getElementById('startRecording').addEventListener('click', () => {
+
+
+
+    });
+
+    document.getElementById('stopRecording').addEventListener('click', () => {
+
+        mediaRecorder.stop();
+
+        document.getElementById('startRecording').disabled = false;
+
+        document.getElementById('stopRecording').disabled = true;
+
+    });
+
+    document.getElementById('goToUploads').addEventListener('click', () => {
+
+        window.location.href = '/audio/list';
+
+    });
+
+    function uploadAudio(audioBlob) {
+
+        const formData = new FormData();
+
+        formData.append('audio', audioBlob, 'voice-recording.webm');
+
+        fetch('/api/upload-audio', {
+
+            method: 'POST',
+
+            body: formData,
+
+            headers: {
+
+                'X-CSRF-TOKEN': '{{ csrf_token() }}',
+
+            },
+
+        })
+
+            .then(response => response.json())
+
+            .then(data => {
+
+                console.log('Audio uploaded successfully:', data);
+
+                window.location.href = '/audio/list';
+
+            })
+
+            .catch(error => console.error('Error uploading audio:', error));
+
+    }
+
 </script>
 @endscript

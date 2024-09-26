b6e3113e04373d63d05f70d9b6efd0a3c040f298
Sorethea OK <sorethea@gmail.com>
2024-09-26 10:20:19 +0700

    commit to hieat

--- resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
+++ resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
@@ -1,14 +1,16 @@
 <x-filament-panels::page>
 <x-filament-panels::form>
     {{$this->form}}
-    <div class="inline-flex gap-2">
-        <x-filament-forms::field-wrapper.label>
-            Voice Recorder
-        </x-filament-forms::field-wrapper.label>
-        <x-filament::button  icon="heroicon-o-microphone" class="w-max" wire:click.prevent="voiceRecord"/>
-        <x-filament::button  icon="heroicon-o-stop" class="w-max" wire:click.prevent="voiceStop"/>
-        <x-filament::button  icon="heroicon-o-play" class="w-max" wire:click.prevent="voicePlay"/>
-    </div>
+    <x-filament-forms::field-wrapper>
+        <x-filament-forms::field-wrapper.label label="Voice Recorder"/>
+        <div class="inline-flex gap-2">
+
+            <x-filament::button  icon="heroicon-o-microphone" class="w-max" wire:click.prevent="voiceRecord"/>
+            <x-filament::button  icon="heroicon-o-stop" class="w-max" wire:click.prevent="voiceStop"/>
+            <x-filament::button  icon="heroicon-o-play" class="w-max" wire:click.prevent="voicePlay"/>
+        </div>
+    </x-filament-forms::field-wrapper>
+
 
     <x-filament-panels::form.actions
         alignment="right"

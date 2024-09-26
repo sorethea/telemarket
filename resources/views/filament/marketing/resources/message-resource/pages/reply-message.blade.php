653f5759a4052232677cfda4d0c36831cd937187
Sorethea OK <sorethea@gmail.com>
2024-09-26 09:45:58 +0700

    commit to hieat

--- resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
+++ resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
@@ -1,7 +1,9 @@
 <x-filament-panels::page>
 <x-filament-panels::form>
     {{$this->form}}
-    <x-filament::button style="{width:200px;}" icon="heroicon-o-microphone"  wire:click.prevent="voiceRecord"/>
+    <x-filament-actions::group>
+        <x-filament::button icon="heroicon-o-microphone"  wire:click.prevent="voiceRecord"/>
+    </x-filament-actions::group>
 
     <x-filament-panels::form.actions
         alignment="right"

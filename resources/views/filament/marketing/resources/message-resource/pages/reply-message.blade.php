7cc532d98ad480733ad6cc631174d7d79dd233c0
Sorethea OK <sorethea@gmail.com>
2024-09-26 10:04:17 +0700

    commit to hieat

--- resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
+++ resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
@@ -2,8 +2,8 @@
 <x-filament-panels::form>
     {{$this->form}}
     <x-filament-forms::field-wrapper>
-        <x-filament::button wire:loading.inline-block icon="heroicon-o-microphone" class="w-max" wire:click.prevent="voiceRecord"/>
-        <x-filament::button wire:loading.inline icon="heroicon-o-play" class="w-max" wire:click.prevent="voicePlay"/>
+        <x-filament::button  icon="heroicon-o-microphone" class="w-max" wire:click.prevent="voiceRecord"/>
+        <x-filament::button  icon="heroicon-o-play" class="w-max inline" wire:click.prevent="voicePlay"/>
     </x-filament-forms::field-wrapper>
     <x-filament-panels::form.actions
         alignment="right"

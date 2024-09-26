f7dd3d31eed6944ea2a96e53d6b0fae0ef2fa54b
Sorethea OK <sorethea@gmail.com>
2024-09-26 10:15:20 +0700

    commit to hieat

--- resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
+++ resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
@@ -1,7 +1,7 @@
 <x-filament-panels::page>
 <x-filament-panels::form>
     {{$this->form}}
-    <div class="inline-flex gap-3">
+    <div class="inline-flex gap-2">
         <x-filament::button  icon="heroicon-o-microphone" class="w-max" wire:click.prevent="voiceRecord"/>
         <x-filament::button  icon="heroicon-o-play" class="w-max" wire:click.prevent="voicePlay"/>
     </div>

9be5a74589441ce2512cbb5a16de5892f1afd19f
Sorethea OK <sorethea@gmail.com>
2024-09-26 15:51:20 +0700

    commit to hieat

--- resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
+++ resources/views/filament/marketing/resources/message-resource/pages/reply-message.blade.php
@@ -4,6 +4,7 @@
     <x-filament-forms::field-wrapper>
         <x-filament-forms::field-wrapper.label>{{trans('market.message.voice_record')}}</x-filament-forms::field-wrapper.label>
         <div class="inline-flex gap-2">
+            <x-gmdi-audio-file ></x-gmdi-audio-file>
             @if($showRecord)
                 <x-filament::button tooltip="{{trans('market.message.record')}}" color="primary" icon="heroicon-o-microphone" class="w-max" wire:click.prevent="voiceRecord"/>
             @endif
@@ -11,6 +12,7 @@
                     <x-filament::button tooltip="{{trans('market.message.stop')}}" color="danger" icon="heroicon-o-stop" class="w-max" wire:click.prevent="voiceStop"/>
             @endif
             @if($showPlay)
+
                     <x-filament::button color="success" tooltip="{{trans('market.message.play')}}" icon="heroicon-o-play" class="w-max" wire:click.prevent="voicePlay"/>
             @endif
 
@@ -45,7 +47,7 @@
                 // document.getElementById('startRecording').disabled = true;
                 //
                 // document.getElementById('stopRecording').disabled = false;
-
+                //
                 // mediaRecorder.addEventListener('dataavailable', event => {
                 //
                 //     audioChunks.push(event.data);

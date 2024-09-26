b36e6e54667b8bb5d9df8036b65bca4406b2eecf
Sorethea OK <sorethea@gmail.com>
2024-09-26 14:59:42 +0700

    commit to hieat

--- app/Filament/Marketing/Resources/MessageResource.php
+++ app/Filament/Marketing/Resources/MessageResource.php
@@ -135,7 +135,7 @@ class MessageResource extends Resource implements HasShieldPermissions
 //                                ->required(),
                             Forms\Components\ViewField::make('voice-record')
                                 ->label("Voice Record")
-                                ->viewData(data: ['chatId'=>$table->getRecordKey(static::$model)])
+                                ->viewData(data: ['chatId'=>$table->getRecordKey(fn($record)=>$record)])
                                 ->view('livewire.voice-recorder')
                         ])
                         ->action(function (array $data,$record){

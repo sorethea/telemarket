f82ea2de919699cce9d55a533498d3efc81ef204
Sorethea OK <sorethea@gmail.com>
2024-10-04 16:56:10 +0700

    commit to hieat

--- app/Filament/Marketing/Resources/MessageResource/Pages/ReplyMessage.php
+++ app/Filament/Marketing/Resources/MessageResource/Pages/ReplyMessage.php
@@ -35,7 +35,7 @@ class ReplyMessage extends Page implements HasForms, HasActions
     protected static string $view = 'filament.marketing.resources.message-resource.pages.reply-message';
 
     public function mount(Request $request){
-        dd($request->url());
+        dd($request->getRequestUri());
     }
 
     public function form(Form $form): Form

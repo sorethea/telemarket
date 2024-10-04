e8e2dbaf4e251d7e7f3e417f715a1ca499c72ac6
Sorethea OK <sorethea@gmail.com>
2024-10-04 14:42:35 +0700

    commit to hieat

--- app/Filament/Marketing/Resources/MessageResource/Pages/ReplyMessage.php
+++ app/Filament/Marketing/Resources/MessageResource/Pages/ReplyMessage.php
@@ -95,7 +95,7 @@ class ReplyMessage extends Page implements HasForms, HasActions
 
     }
     public function saveVoice(): void{
-        dd($this->audio);
+        dd($this);
         //$fileName = Str::random(32).".wav";
         //dd($this->audio);
         //Storage::put($fileName,file_get_contents($this->audio));

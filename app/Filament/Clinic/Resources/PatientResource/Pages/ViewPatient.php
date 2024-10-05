0135200874035afd74672feb8f882ef1cb520869
sorethea <sorethea@gmail.com>
2024-10-05 16:27:08 +0700

    TheA Clinic First Commit

--- app/Filament/Clinic/Resources/PatientResource/Pages/ViewPatient.php
+++ app/Filament/Clinic/Resources/PatientResource/Pages/ViewPatient.php
@@ -2,9 +2,10 @@
 
 namespace App\Filament\Clinic\Resources\PatientResource\Pages;
 
+use App\Filament\Clinic\Resources\PatientResource;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewPatient extends ViewRecord
 {
-
+    protected static string $resource = PatientResource::class;
 }

1e58c83a56e4ced6a609f12615cacd70e499d261
sorethea <sorethea@gmail.com>
2024-10-05 14:04:08 +0700

    TheA Clinic First Commit

--- app/Filament/Clinic/Resources/PatientResource.php
+++ app/Filament/Clinic/Resources/PatientResource.php
@@ -19,7 +19,7 @@ class PatientResource extends Resource
 {
     protected static ?string $model = Patient::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-users';
 
     public static function form(Form $form): Form
     {

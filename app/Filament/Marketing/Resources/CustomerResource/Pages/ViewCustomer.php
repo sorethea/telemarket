26086107b2e8f04398a6d793d04d8cdc703868ac
Sorethea OK <sorethea@gmail.com>
2024-10-07 15:00:48 +0700

    commit to hieat

--- app/Filament/Marketing/Resources/CustomerResource/Pages/ViewCustomer.php
+++ app/Filament/Marketing/Resources/CustomerResource/Pages/ViewCustomer.php
@@ -4,10 +4,110 @@ namespace App\Filament\Marketing\Resources\CustomerResource\Pages;
 
 use App\Filament\Marketing\Resources\CustomerResource;
 use Filament\Actions;
+use Filament\Actions\Action;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\ViewField;
 use Filament\Resources\Pages\CreateRecord;
 use Filament\Resources\Pages\ViewRecord;
+use Telegram\Bot\FileUpload\InputFile;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class ViewCustomer extends ViewRecord
 {
     protected static string $resource = CustomerResource::class;
+
+    public $showRecord;
+    public $showStop;
+
+    public function voiceRecord(): void
+    {
+        $this->dispatch('voiceRecordStart',['message'=>"This is a dispatch."]);
+        $this->showStop = true;
+    }
+
+    public function voiceStop(): void
+    {
+        $this->dispatch('voiceRecordStop',['message'=>"This is a dispatch."]);
+        $this->showStop = false;
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return[
+            Action::make("text-reply")
+                ->form([
+                    MarkdownEditor::make("text")
+                        ->label(trans("market.message.content"))
+                        ->required(),
+                ])
+                ->hiddenLabel(true)
+                ->icon('heroicon-o-chat-bubble-oval-left-ellipsis')
+                ->tooltip(trans('market.message.voice_reply'))
+                ->modalSubmitActionLabel('Reply')
+                ->action(function ($data){
+                    $telegram = Telegram::bot($this->record->bot);
+                    $telegram->sendMessage([
+                        'chat_id'=>$this->record->customer_id,
+                        'text'=>$data["text"],
+                    ]);
+                    $replyMessage = new \App\Models\ReplyMessage();
+                    $replyMessage->status="sent";
+                    $replyMessage->type="text";
+                    $replyMessage->text=$data["text"];
+                    $this->record->replyMessages()->save($replyMessage);
+                })
+                ->modal(),
+
+            Action::make("file-reply")
+                ->form([
+                    FileUpload::make("files")
+                        ->multiple()
+                        ->label(trans("market.message.file"))
+                        ->required(),
+                ])
+                ->hiddenLabel(true)
+                ->icon('heroicon-o-paper-clip')
+                ->tooltip(trans('market.message.voice_reply'))
+                ->modalSubmitActionLabel('Reply')
+                ->action(function ($data){
+                    $telegram = Telegram::bot($this->record->bot);
+                    $files = $data["files"];
+                    foreach ($files as $file){
+                        $telegram->sendDocument([
+                            'chat_id'=>$this->record->customer_id,
+                            'document'=>InputFile::create("storage/".$file),
+                        ]);
+                        $replyMessage = new \App\Models\ReplyMessage();
+                        $replyMessage->status="sent";
+                        $replyMessage->type="document";
+                        $replyMessage->file=$file;
+                        $this->record->replyMessages()->save($replyMessage);
+                    }
+                })
+                ->modal(),
+            Action::make("voice-reply")
+                ->form([
+                    ViewField::make('voice-reply')
+                        ->view("livewire.voice-reply")
+                ])
+                ->hiddenLabel(true)
+                ->icon('heroicon-o-microphone')
+                ->tooltip(trans('market.message.voice_reply'))
+                ->modalSubmitActionLabel('Reply')
+                ->action(function ($record){
+                    $record->replyMessages->where("type","voice")->where("status","draft")->each( function ($reply) use($record){
+                        $telegram = Telegram::bot($record->bot);
+                        $telegram->sendVoice([
+                            'chat_id'=>$record->customer_id,
+                            'voice'=>InputFile::create('storage/'.$reply->file),
+                        ]);
+                        $reply->status = "sent";
+                        $reply->save();
+                    });
+                })
+                ->modal(),
+
+        ];
+    }
 }

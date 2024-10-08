2bc9a239a6c0fba4c10b8763121c156553f17610
Sorethea OK <sorethea@gmail.com>
2024-10-08 10:11:59 +0700

    commit to hieat

--- app/Filament/Marketing/Resources/CustomerResource/RelationManagers/MessagesRelationManager.php
+++ app/Filament/Marketing/Resources/CustomerResource/RelationManagers/MessagesRelationManager.php
@@ -52,15 +52,15 @@ class MessagesRelationManager extends RelationManager
                 //
             ])
             ->headerActions([
-                Tables\Actions\CreateAction::make(),
+                //Tables\Actions\CreateAction::make(),
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                //Tables\Actions\EditAction::make(),
+                //Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                    //Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
     }
